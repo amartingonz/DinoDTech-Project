b798835ac2e64e5cbc67eb8693d76aebd30417d6
Alejandro <amartingdaw@gmail.com>
2022-11-18 13:38:58 +0100

    Estructura v0.1

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    include './lib/header.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/home.css">
+    <title>DinoDTech</title>
+</head>
+<body>
+    <div class="caja-imagen">
+        <img src="" alt="">
+    </div>
+    <div class="caja-testimonios">
+        <div class="caja-izq">a</div>
+        <div class="caja-cen">a</div>
+        <div class="caja-der">a</div>
+    </div>
+</body>
+</html>
+<?php
+    include './lib/footer.php';
+?>
---
+++ lib/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/header.css">
+    <title>DinoDTech</title>
+</head>
+<body>
+    <header class="header">
+        <a href="index.html"><img src="./img/logo.png" alt="logo" width="80px"></a>
+        <nav class="menu">
+            <a href="dweb.html">Desarrollo Web</a>
+            <a href="hosting.html">Hosting</a>
+            <a href="seo.html">SEO</a>
+            <a href="gsmedia.html">Gestión de Redes Sociales</a>
+            <a href="blog.html">Blog</a>
+        </nav>
+
+    </header>
+</body>
+</html>
