dc07d1c6c573fc3dc08310e6a533464301a90e3c
Alejandro <amartingdaw@gmail.com>
2022-11-19 14:17:04 +0100

    version1

--- lib/footer.php
+++ lib/footer.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/footer.css">
-    <title>Document</title>
-
-</head>
-<body>
-    <footer class="footer">
+    <div class="footer">
         <a href="index.html"><img src="./img/logo.png" alt="logo" width="80px"></a>
         <a href="contacto.php">Contacto</a>
         <nav class="iconos-media">
@@ -19,6 +8,6 @@
         </nav>
         <a href="aviso-legal.php">Aviso Legal</a>
         <a href="cookies.php">Políticas de Privacidad y Cookies</a>
-    </footer>
-</body>
-</html>
+    </div>
+
+    
--- lib/header.php
+++ lib/header.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/header.css">
-    <title>DinoDTech</title>
-</head>
-<body>
-    <header class="header">
-        <a href="index.html"><img src="./img/logo.png" alt="logo" width="80px"></a>
+    <div class="header">
+        
         <nav class="menu">
+            <a href="index.html"><img src="./img/logo.png" alt="logo"></a>
             <a href="dweb.html">Desarrollo Web</a>
             <a href="hosting.html">Hosting</a>
             <a href="seo.html">SEO</a>
@@ -18,6 +9,4 @@
             <a href="blog.html">Blog</a>
         </nav>
 
-    </header>
-</body>
-</html>
+    </div>
