537f243357116a49c3e3968a571cda25894202ae
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-02-03 14:40:43 +0100

    ARREGLAR MEDIA QUERYS
    ARREGLAR MEDIA QUERYS

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 
 
-function menu(){
-    let nav = document.getElementById('menu');
+// $('#boton-menu').click(function(){
+//     $('nav ul li').css('display','block')
+// })
 
-    nav.style.visibility = "visible";
-
-}
+// $('#boton-menu').click(function(){
+//     $('nav ul li').hide(function(){
+//         $('nav ul li').css('display','block')
+//     })
+// })
+var estado = true;
+$('#boton-menu').click(function(){
+    if(estado){
+        $('#menu ul li').show(function(){
+            $('#menu ul li').css('display','block');
+            estado = false;
+    })
+    }else{
+        $('#menu ul li').hide(function(){
+            $('#menu ul li').css('display','none');
+            estado = true;
+    })
+    }
+    
+    
+})
