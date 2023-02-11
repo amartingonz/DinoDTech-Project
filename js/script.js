826cb7de5ea0e5a4bcdba229ca4db535b6d17fa3
Alejandro <amartingdaw@gmail.com>
2023-02-11 18:36:50 +0100

    funciona el responsive en todas las páginas

--- js/script.js
+++ js/script.js
@@ -9,19 +9,33 @@
 //         $('nav ul li').css('display','block')
 //     })
 // })
-var estado = true;
-$('#boton-menu').click(function(){
-    if(estado){
-        $('#menu ul li').show(function(){
-            $('#menu ul li').css('display','block');
-            estado = false;
-    })
-    }else{
-        $('#menu ul li').hide(function(){
-            $('#menu ul li').css('display', "");
-            estado = true;
-    })
-    }
+// var estado = true;
+// $('#boton-menu').click(function(){
+//     if(estado){
+//         $('#menu ul li').show(function(){
+//             $('#menu ul li').css('display','block');
+//             estado = false;
+//     })
+//     }else{
+//         $('#menu ul li').hide(function(){
+//             $('#menu ul li').css('display', "");
+//             estado = true;
+//     })
+//     }
     
     
-})
+// })
+
+var checkbox = document.getElementById('check');
+var h1 = document.getElementById('titulo');
+checkbox.addEventListener("change", validaCheckbox, false);
+// Funcion para que si el checkbox esta activado del menu se quite el H1
+function validaCheckbox()
+{
+  var checked = checkbox.checked;
+  if(checked){
+    h1.style.display = "none";
+  }else{
+    h1.style.display = "";
+  }
+}
