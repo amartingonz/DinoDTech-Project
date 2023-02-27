e1b8947e8b151fe602050bf1a1c618e54349f5ca
Alejandro <amartingdaw@gmail.com>
2023-02-27 18:45:23 +0100

    COMENTARIOS E IMAGENES

--- js/script.js
+++ js/script.js
@@ -1,31 +1,4 @@
 
-
-// $('#boton-menu').click(function(){
-//     $('nav ul li').css('display','block')
-// })
-
-// $('#boton-menu').click(function(){
-//     $('nav ul li').hide(function(){
-//         $('nav ul li').css('display','block')
-//     })
-// })
-// var estado = true;
-// $('#boton-menu').click(function(){
-//     if(estado){
-//         $('#menu ul li').show(function(){
-//             $('#menu ul li').css('display','block');
-//             estado = false;
-//     })
-//     }else{
-//         $('#menu ul li').hide(function(){
-//             $('#menu ul li').css('display', "");
-//             estado = true;
-//     })
-//     }
-    
-    
-// })
-
 var checkbox = document.getElementById('check');
 var h1 = document.getElementById('titulo');
 checkbox.addEventListener("change", validaCheckbox, false);
