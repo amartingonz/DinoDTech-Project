ad8dae90a75b3ad5b1f23ac54cb9d9ec60827c2a
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-03-02 23:25:08 +0100

    del
    del

--- dist/index.d552f17a.js
+++
@@ -1,2 +0,0 @@
-var checkbox=document.getElementById("check"),h1=document.getElementById("titulo");function validaCheckbox(){var e=checkbox.checked;h1.style.display=e?"none":""}checkbox.addEventListener("change",validaCheckbox,!1);
-//# sourceMappingURL=index.d552f17a.js.map
